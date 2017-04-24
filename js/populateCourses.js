65f48c72266f69332c1591e310c6121a7a5ecff6
tonybumatay <Tony@Tonys-MacBook-Pro.local>
2017-04-23 21:15:23 -0500

    updating DOM formatting

--- js/populateCourses.js
+++ js/populateCourses.js
@@ -25,21 +25,97 @@ function generateTable() {
   var dept = $("#dept option:selected").text();
   var days = $("#days option:selected").text();
   var queryString = "html heroku blah" + start + "/";
-  $('<div>' + queryString + '</div>').appendTo(document.getElementById("body"));
-  $('<div>' + end + '</div>').appendTo(document.getElementById("body"));
-  $('<div>' + dept + '</div>').appendTo(document.getElementById("body"));
-  $('<div>' + days + '</div>').appendTo(document.getElementById("body"));
+  //$('<div>' + queryString + '</div>').appendTo(document.getElementById("body"));
+  //$('<div>' + end + '</div>').appendTo(document.getElementById("body"));
+  //$('<div>' + dept + '</div>').appendTo(document.getElementById("body"));
+  //$('<div>' + days + '</div>').appendTo(document.getElementById("body"));
 
 
   //$( "#myselect option:selected" ).text();
   // var table = document.createElement("TABLE");
-  $("#display tr").remove();
+
+  var $div = $("<div>", {id: "singleCourse", "class": "a"});//
+  var $courses1 = $("<div>", {id: "firstRow", "class": "col-sm-12"});//first row
+  var $courses2 = $("<div>", {id: "secondRow", "class": "col-sm-12"});//second row
+  var $courses3 = $("<div>", {id: "thirdRow", "class": "col-sm-12"});//third row
+  var $courses4 = $("<div>", {id: "fourthRow", "class": "col-sm-12"});//third row
+
+  $div.append($courses1);
+  $div.append($courses2);
+  $div.append($courses3);
+  $div.append($courses4);
+  $div.appendTo(document.getElementById("body"));
+
+
+
+
+
+
+
+
+
+
+
+
+  var codeAttr, descriptionAttr, courseTitleHeader, daysAttr, creditsAttr, timeAttr, professorAttr;
+
+  $("#singleCourse").appendTo(document.getElementById("display"));
+
+  //$("#display tr").remove();
   $.each(data, function(key, val) {
-      $('<tr><td>'+key+'</td><td id="'+key+'">'+val+'</td><tr>').appendTo(document.getElementById("display"));
+      //$('<tr><td>'+key+'</td><td id="'+key+'">'+val+'</td><tr>').appendTo(document.getElementById("display"));
+
+      switch(key){
+        case "Course Code":
+          //Course Code
+          var code = key + ": " + val;
+          codeAttr = $('<h3>' + code + '</h3>').attr('id', 'code').attr('class', 'testClass').attr('class', 'col-md-6');
+          break;
+        case "Description":
+          //Description
+          var description = key + ": " + val;
+          descriptionAttr = $('<p>' + description + '</p>').attr('id', 'description');
+          break;
+        case "Title":
+          //Course Title
+          var title = val;
+          courseTitleHeader = $('<h2>' + title + '</h2>').attr('id', 'courseTitle');
+
+        case "Date":
+          //Date
+          var days = "Days: " + val;
+          daysAttr = $('<h4>' + days + '</h4>').attr('id', 'days').attr('class', 'col-md-4');
+
+        case "Credits":
+          //Credits
+          var credits = key + ": " + val;
+          creditsAttr = $('<h4>' + credits + '</h4>').attr('id', 'credits').attr('class', 'col-md-4');
+
+        case "Time":
+          //Time
+          var time = key + ": " + val;
+          timeAttr = $('<h4>' + time + '</h4>').attr('id', 'time').attr('class', 'col-md-4');
+
+        case "Instructor":
+          //Professor
+          var prof = key + ": " + val;
+          professorAttr = $('<h3>' + prof + '</h3>').attr('id', 'professor').attr('class', 'col-md-6');
+      }
+
+
   });
+
+  $("#firstRow").append(courseTitleHeader);
+  $("#secondRow").append(professorAttr);
+  $("#secondRow").append(codeAttr);
+  $("#thirdRow").append(daysAttr);
+  $("#thirdRow").append(timeAttr);
+  $("#thirdRow").append(creditsAttr);
+  $("#fourthRow").append(descriptionAttr);
+
   $.getJSON( "https://protected-lowlands-84461.herokuapp.com/universities", function( data ) {
     $.each( data, function( key, val ) {
-        $('<tr><td>'+key+'</td><td id="'+key+'">'+val+'</td><tr>').appendTo(document.getElementById("display"));
+        //$('<tr><td>'+key+'</td><td id="'+key+'">'+val+'</td><tr>').appendTo(document.getElementById("display"));
     });
   });
 }
